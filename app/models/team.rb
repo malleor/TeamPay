1978dcc957d144996cd617131db75d0fcadf6ce8
Janusz Lenar <malleor@gmail.com>
2014-09-20 14:09:14 +0200

    Added team leader

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
-  has_many :members, class_name: 'User'
+  has_many   :members, class_name: 'User'
+  belongs_to :leader,  class_name: 'User'
 end
---
+++ db/migrate/20140920113256_add_team_leader.rb
@@ -0,0 +1,5 @@
+class AddTeamLeader < ActiveRecord::Migration
+  def change
+    add_column :teams, :leader_id, :integer
+  end
+end
